61c35dab02e95e1737406e8e04e81b61aa9d2545
Peter Zhang <jzhang046@e.ntu.edu.sg>
2018-02-23 03:02:46 +0800

    Occupy whole line in the board area.

--- Tic-Tac-Toe/Display.py
+++ Tic-Tac-Toe/Display.py
@@ -4,54 +4,93 @@ import numpy as np
 
 class Display:
 
-    term = None
+    term = None #blessings Terminal object.
 
-    def __init__(self):
+    def __init__(self, size):
         self.term = Terminal()
-        print(term.enter_fullscreen)
+        print(self.term.enter_fullscreen)
+        for num in range(2 * size + 1):
+            print("")
 
     def printBoard(self, board):
 
         rowIndex = -1
         colIndex = -1
 
-        self.printTopButtLine(board.shape[1])
+        boardRows = board.shape[0]
+        boardCols = board.shape[1]
 
-        for row in board:
-            rowIndex += 1
+        #Always print on the top.
+        with self.term.location(0, 0):
 
-            for value in np.nditer(row):
+            self.printTopButtLine(boardCols)
 
-                colIndex += 1
-                if value == 0:
-                    sys.stdout.write('|%2d,%2d' % (rowIndex, colIndex))
-                elif value > 0:
-                    sys.stdout.write('|' + self.term.on_bright_blue + \
-                        '  X  ' + self.term.normal)
+            for row in board:
+                rowIndex += 1
+
+                colPlaceCounter = 0;
+                for value in np.nditer(row):
+
+                    colIndex += 1
+                    if value == 0:
+                        #Further developments: handle more digits...?
+                        sys.stdout.write('|%2d,%2d' % (rowIndex, colIndex))
+                    elif value > 0:
+                        sys.stdout.write('|' + self.term.on_bright_blue + \
+                            '  X  ' + self.term.normal)
+                    else:
+                        #value < 0
+                        sys.stdout.write('|' + self.term.on_bright_yellow + \
+                            '  O  ' + self.term.normal)
+
+                    #Every write occupy 6 positions.
+                    colPlaceCounter += 6
+
+                #End of informations...
+                sys.stdout.write('|')
+                colPlaceCounter += 1
+
+                #Occupy the whole line.
+                self.occupyTilEnd(colPlaceCounter)
+
+                #Print seperation line in between or buttom.
+                #Seperation lines were designed to have same length as data line,
+                #   thus still passing colPlaceCounter here.
+                if rowIndex < board.shape[0] - 1:
+                    self.printMidSepLine(boardCols)
                 else:
-                    #value < 0
-                    sys.stdout.write('|' + self.term.on_bright_yellow + \
-                        '  O  ' + self.term.normal)
+                    self.printTopButtLine(boardCols)
 
-            sys.stdout.write('|\n')
+                colIndex = -1
 
-            if rowIndex < board.shape[0] - 1:
-                self.printMidSepLine(board.shape[1])
-            else:
-                self.printTopButtLine(board.shape[1])
+            self.printTopButtLine(boardCols)
 
-            colIndex = -1
+    def occupyTilEnd(self, colPlaceCounter):
+        currentWidth = self.term.width
+        while colPlaceCounter < currentWidth:
+            sys.stdout.write(' ')
+            colPlaceCounter += 1
+        sys.stdout.write('\n')
 
     def printMidSepLine(self, colNum):
         #Input: number of cols.
         for tempNum in range(colNum):
             sys.stdout.write('|-----')
-        sys.stdout.write('|\n')
+        sys.stdout.write('|')
+        self.occupyTilEnd(colNum * 6 + 1)
 
     def printTopButtLine(self, colNum):
         for tempNum in range(colNum):
             sys.stdout.write('------')
-        sys.stdout.write('-\n')
+        sys.stdout.write('-')
+        self.occupyTilEnd(colNum * 6 + 1)
 
     def warning(self, warningStr):
         print(self.term.red + warningStr + self.term.normal)
+
+    def showBold(self, boldStr):
+        print(self.term.bold + self.term.underline + boldStr + self.term.normal)
+
+    def exit(self):
+        print(self.term.exit_fullscreen)
+        sys.exit()
