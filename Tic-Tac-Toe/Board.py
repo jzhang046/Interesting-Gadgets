46878abf438c42078538e199411a52eb9a2d0a2e
Peter Zhang <jzhang046@e.ntu.edu.sg>
2018-02-23 01:14:03 +0800

    Completed functions for basic playing. Next: better display.

---
+++ Tic-Tac-Toe/Board.py
@@ -0,0 +1,125 @@
+import numpy as np
+import Display as D
+
+class Board:
+
+    size = -1 #Size of the board.
+    board = None #Current status of the board.
+    XPlayer = None #Name of player using 'X'
+    OPlayer = None #Name of player using 'O
+    lastPosi = None #Last player entered coordinates. [row, col].
+
+    def __init__(self):
+
+        #Prompt for size of board. Minimal size should be 3.
+        validInt = False
+        while self.size < 3:
+
+            if validInt:
+                print("INVALID SIZE. Size should >= 3. ")
+
+            try:
+                self.size = int(raw_input("Enter board size n: "))
+                validInt = True
+            except KeyboardInterrupt:
+                raise
+            except:
+                print("INVALID INPUT. Please enter a valid integer. ")
+                validInt = False
+
+        self.board = np.zeros((self.size, self.size), np.int8)
+
+        #Prompt for players' name.
+        self.XPlayer = raw_input("Enter name for player 1: ")
+        self.OPlayer = raw_input("Enter name for player 2: ")
+
+    def show(self):
+        D.printBoard(self.board)
+
+    def checkWin(self):
+        #Would be called after getting valid self.lastPosi.
+
+        rowPosi = self.lastPosi[0]
+        colPosi = self.lastPosi[1]
+
+        def getBoardValueAt(board, row, col):
+            #Only for checkMin use.
+            if row not in range(board.shape[0]) or col not in range(board.shape[1]):
+                return 0
+                #return value of 0 will always be different with putted position.
+            else:
+                return int(board[row, col])
+
+        #Loop over -1, 0, 1 to check all positions around a position.
+        #Win should always occur after one step around last placed position.
+        for colOffset in range(-1, 2):
+            for rowOffset in range(-1, 2):
+
+                #Check if the index points to itself.
+                if colOffset == 0 and rowOffset == 0:
+                    continue
+                else:
+
+                    checkedValue = getBoardValueAt(self.board, \
+                        rowPosi + rowOffset, colPosi + colOffset)
+                    origValue = getBoardValueAt(self.board, \
+                        rowPosi, colPosi)
+
+                    if checkedValue != origValue:
+                        #No need to check further. Skipping.
+                        continue
+                    else:
+                        #Found 2 in a row. Checking 3nd position.
+                        oppoValue = getBoardValueAt(self.board, \
+                            rowPosi - rowOffset, colPosi - colOffset)
+                        furtherValue = getBoardValueAt(self.board, \
+                            rowPosi + 2 * rowOffset, colPosi + 2 * colOffset)
+
+                        if oppoValue == origValue or furtherValue == origValue:
+                            return True
+
+        #After all looped position without returning true...
+        return False
+
+    def getNextX(self):
+        self.lastPosi = self.getNext( \
+            "%s, choose a coordinate to put a 'X': " % self.XPlayer)
+        self.board[self.lastPosi[0], self.lastPosi[1]] += 1
+
+    def getNextO(self):
+        self.lastPosi = self.getNext( \
+            "%s, choose a coordinate to put a 'O': " % self.OPlayer)
+        self.board[self.lastPosi[0], self.lastPosi[1]] -= 1
+
+    def getNext(self, ppStr):
+
+        isValid = False
+
+        while not isValid:
+            self.show()
+            posiStr = raw_input(ppStr)
+
+            try:
+                row = int(posiStr.split(',')[0])
+                col = int(posiStr.split(',')[1])
+            except KeyboardInterrupt:
+                raise
+            except:
+                print("INVALID INPUT. Please enter 2 integers seperated by comma. ")
+                isValid = False
+                continue
+
+            if row not in range(self.board.shape[0]) or \
+                    col not in range(self.board.shape[1]):
+                print("INVALID INPUT. Position out of range. ")
+                isValid = False
+                continue
+
+            if self.board[row, col] != 0:
+                print("INVALID INPUT. Occupied position. ")
+                isValid = False
+                continue
+
+            isValid = True
+
+        return [row, col]
--- Tic-Tac-Toe/nTTT.py
+++ Tic-Tac-Toe/nTTT.py
@@ -1,45 +1,29 @@
-import numpy as np 
-import Display as D
+from Board import Board
 
-#Get the board size. 
-size = -1
-validInt = False
-while size < 3: 
+#Initialize
+tttb = Board()
 
-    if validInt: 
-        print("INVALID SIZE. Size should >= 3. ")
-        
-    try: 
-        size = int(raw_input("Enter board size n: "))
-        validInt = True
-    except KeyboardInterrupt: 
-        raise
-    except: 
-        print("INVALID INPUT. Please enter a valid integer. ")
-        validInt = False
-
-board = np.zeros((size, size), np.int8)
-D.printBoard(board)
-
+#Start playing.
 winned = False
 isX = True
-while not winned: 
-    
-    #TODO: error handling for raw_input. 
-    if isX: 
-        posiStr = raw_input("%s, choose a coordinate to put a 'X': " % 'X')
-    else: 
-        posiStr = raw_input("%s, choose a coordinate to put a 'O': " % 'O')
+while not winned:
 
-    rowPosi = int(posiStr.split(',')[0])
-    colPosi = int(posiStr.split(',')[1])
+    if isX:
+        tttb.getNextX()
+    else:
+        tttb.getNextO()
 
-    #TODO: check if it's a occupied place. 
-    if isX: 
-        board[rowPosi, colPosi] += 1
-    else: 
-        board[rowPosi, colPosi] -= 1
+    tttb.show()
 
-    D.printBoard(board)
+    winned = tttb.checkWin()
 
     isX = not isX
+
+#Playing ended.
+#Here isX should be the one who lose.
+if (isX):
+    winner = tttb.OPlayer
+else:
+    winner = tttb.XPlayer
+
+print("Congratulations %s! You have won. " % winner)
